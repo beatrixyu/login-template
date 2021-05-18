f8c3067b8dfe431ad8e209276ff592664421adb7
beatrixyu <beatrixyu@126.com>
2021-05-18 10:10:21 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
-  <div id="app">
-      <div>
-        <img alt="company logo" src="../assets/images/logo.png">
+  <div id="app" class="grid justify-items-stretch">
+      <div class="flex">
+      <div class="w-1/2">
+        <img class="w-32" alt="company logo" src="../assets/images/logo.png">
       </div>
-      <div>
-          <div><span><img alt="trust" src="../assets/images/safe.png"/></span></div>
+      <div class="flex justify-center items-center w-1/2">
+          <div  class="w-10 flex"><img alt="trust" src="../assets/images/safe.png"/></div>
           <div>
               <div class="flex flex-row" >
                   <img class="w-10 h-auto" alt="stars" src="../assets/images/yellowStar.png"/>
@@ -15,11 +16,13 @@
               </div>
               <div class="flex flex-row">
                   <p> 4.85 / 5.00</p>
-                  <img alt="e" src="../assets/images/e.png"/>
+                  <img class="w-6" alt="e" src="../assets/images/e.png"/>
                   <p><strong>TRUSTED</strong>SHOPS</p>
               </div>
           </div>
       </div>
+      </div>
+
 
   </div>
 </template>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="login">
     <h1>Register</h1>
-     <form v-on:reset="resetFields" v-on:submit.prevent="submitForm" class="col-span-1 lg:col-span-6">
-       <div class="form-row">
+     <form v-on:reset="resetFields" v-on:submit.prevent="submitForm" class="border-4">
+       <div class="form-row border-2">
          <div class="form-field">
           <!-- <label for="first-name">First Name</label> -->
           <input type="text" id="first-name" v-model.trim="fullname" placeholder="Vorname*"/>
         </div>
       </div>
-      <div class="form-row">
+      <div class="form-row border-2">
         <div class="form-field">
           <!-- <label for="email">Email</label> -->
           <input type="text" id="email" v-model.trim="email" placeholder="E-Mail Adresse*"/>
         </div>
       </div>
-       <div class="form-row">
+       <div class="form-row border-2">
         <div class="form-field">
           <!-- <label for="password">Password</label> -->
           <input type="password" id="password" v-model.trim="password" placeholder="Passwort*"/>
