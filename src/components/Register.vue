a4fad2a4c412a16f00e703e4373a3aa1c6464b39
beatrixyu <beatrixyu@126.com>
2021-05-18 18:22:46 +0200

    main register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="register" class="h-full">
-    <div class="border-b-2 border-gray-200">
+    <div class="border-b border-gray-200">
       <div class="pl-6 pr-6">
       <h1 class="text-xl leading-relaxed mt-12 font-bold">ICH BIN NEU HIER</h1>
       <h3 class="text-gray-400 text-xs">Jetzt Dein Kundenkonto erstellen.</h3>
       <div class="mt-5">
           <div class="flex items-center">
-            <span class="inline-block align-middle">
+            <span class="inline-block align-middle h-full">
               <img src="../assets/images/blackStar.png" alt="star" class="w-5 mr-3">
             </span>
             <p class="text-xs text-gray-600 leading-8">Automatisch <strong class="text-black">Punkte</strong> für Deinen Einkauf <strong class="text-black"> Sammeln</strong></p>
@@ -22,25 +22,25 @@
     </div>
       </div>
      <form v-on:reset="resetFields" v-on:submit.prevent="submitForm" class="mt-7 pl-6 pr-6">
-       <div class="form-row border-2 border-gray-300 mt-5">
-         <div class="form-field h-12">
+       <div class="form-row border border-gray-400 mt-5">
+         <div class="form-field h-10">
           <!-- <label for="first-name">First Name</label> -->
-          <input class="placeholder-gray-400 h-full w-full focus:ring-1 focus:ring-purple-600 pl-4" type="text" id="first-name" v-model.trim="fullname" placeholder="Vorname*"/>
+          <input class="placeholder-gray-400 h-full w-full focus:ring-1 focus:ring-purple-600 pl-4 text-xs" type="text" id="first-name" v-model.trim="fullname" placeholder="Vorname*"/>
         </div>
       </div>
-      <div class="form-row border-2 border-gray-300 mt-5">
-        <div class="form-field h-12">
+      <div class="form-row border border-gray-400 mt-5">
+        <div class="form-field h-10">
           <!-- <label for="email">Email</label> -->
-          <input class="placeholder-gray-400 h-full w-full pl-4" type="text" id="email" v-model.trim="email" placeholder="E-Mail Adresse*"/>
+          <input class="placeholder-gray-400 h-full w-full pl-4 text-xs" type="text" id="email" v-model.trim="email" placeholder="E-Mail Adresse*"/>
         </div>
       </div>
-       <div class="form-row border-2 border-gray-300 mt-5">
-        <div class="form-field h-12">
+       <div class="form-row border border-gray-400 mt-5">
+        <div class="form-field h-10">
           <!-- <label for="password">Password</label> -->
-          <input class="placeholder-gray-400 h-full w-full pl-4" type="password" id="password" v-model.trim="password" placeholder="Passwort*"/>
+          <input class="placeholder-gray-400 h-full w-full pl-4 text-xs" type="password" id="password" v-model.trim="password" placeholder="Passwort*"/>
         </div>
       </div>
-       <button class="h-12 bg-green-300 rounded-sm w-full mt-5 uppercase text-white bg-color-primary font-extrabold" type="submit" v-bind:disabled="!formIsValid">registrieren</button>
+       <button class="h-11 bg-green-300 rounded-sm w-full mt-5 uppercase text-white primary font-extrabold" type="submit" v-bind:disabled="!formIsValid">registrieren</button>
      </form>              
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
     theme: {
-     textColor: {
-       'primary': '#AED23B',
-       'secondary': '#40B5E1',
-       'danger': '#FC5763',
+     extend: {
+         colors:{
+            'primary': '#AED23B',
+            'secondary': '#40B5E1',
+            'danger': '#FC5763',     
+         },
+         fontFamily:{
+             body: ['Poppins','Roboto']
+         }
      }
     }
   }
